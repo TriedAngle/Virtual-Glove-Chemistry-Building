67e736595b27683827a40033ac0bcc1a36a295a9
Sebastian Strobl <Sebastian@Strobl.net>
2020-01-05 03:54:56 +0100

    fix structure + scripts + objects

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,175 @@
+﻿//
+//            ,----,                 
+//          ,/   .`|                 
+//        ,`   .'  :    ,---,        
+//      ;    ;     /   '  .' \       
+//    .'___,/    ,'   /  ;    '.     
+//    |    :     |   :  :       \    
+//    ;    |.';  ;   :  |   /\   \   
+//    `----'  |  |   |  :  ' ;.   :  
+//        '   :  ;   |  |  ;/  \   \ 
+//        |   |  '   '  :  | \  \ ,' 
+//        '   :  |   |  |  '  '--'   
+//        ;   |.'    |  :  :         
+//        '---'      |  | ,'         
+//                   `--''           
+//                               
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.IO.Ports;
+
+
+public class HandController : MonoBehaviour
+{
+    private SerialPort _serialPort;
+    private Thread _serialReader;
+
+    private bool windows = false;
+    private string _serialPortPath = "";
+    private const int BaudRate = 9600;
+    private const int ValueAmount = 11;
+    private const char ValueSeparator = '|';
+
+    private bool isRunning;
+    private const int ThreadSleepInMillis = 10;
+
+    private float[] _values;
+
+    public static bool Grabbing = false;
+
+    // Fingers
+    // Thumb Finger
+    public Transform hand;
+    public Transform thumb1;
+    public Transform thumb2;
+
+    // Index Finger
+    public Transform index1;
+    public Transform index2;
+    public Transform index3;
+
+    // Middle Finger
+    public Transform middle1;
+    public Transform middle2;
+    public Transform middle3;
+
+    // Ring Finger
+    public Transform ring1;
+    public Transform ring2;
+    public Transform ring3;
+
+    // Pinky Finger
+    public Transform pinky1;
+    public Transform pinky2;
+    public Transform pinky3;
+
+    // Current use for each Data Value 
+    //
+    // 0 = Thumb Angle      Angle
+    // 1 = Index Finger     Angle
+    // 2 = Middle Finger    Angle
+    // 3 = Ring Finger      Angle
+    // 4 = Pinky Finger     Angle
+    //
+    // 5 = Hand Angle X
+    // 6 = Hand Angle Y
+    // 7 = Hand Angle Z
+    //
+    // 8 = Hand Force X
+    // 9 = Hand Force Y
+    // 10 = Hand Force Z
+    //
+    // Total Amount of Values: 11 
+
+    private void Start()
+    {
+        // Initialize Variables
+        isRunning = true;
+        _values = new float[ValueAmount];
+
+        // Creating Serial
+        _serialPortPath = windows ? "COM0" : "/dev/ttyUSB0";
+        _serialPort = new SerialPort(_serialPortPath, BaudRate);
+        _serialPort.Open();
+
+        //Creating Thread
+        _serialReader = new Thread(ReadData);
+        _serialReader.Start();
+    }
+
+    public void Update()
+    {
+        // Rotate Hand
+        hand.transform.Rotate(-_values[5] * Time.deltaTime, -_values[6] * Time.deltaTime, -_values[7] * Time.deltaTime );
+        
+        // Rotate Fingers
+        thumb1.transform.localRotation = Quaternion.Euler(_values[0], 0f, 0f);
+        thumb2.transform.localRotation = Quaternion.Euler(2f * _values[0], 0f, 0f);
+
+        index1.transform.localRotation = Quaternion.Euler(_values[1], 0f, 0f);
+        index2.transform.localRotation = Quaternion.Euler(2f * _values[1], 0f, 0f);
+        index3.transform.localRotation = Quaternion.Euler(3f * _values[1], 0f, 0f);
+
+        middle1.transform.localRotation = Quaternion.Euler(_values[2], 0f, 0f);
+        middle2.transform.localRotation = Quaternion.Euler(2f * _values[2], 0f, 0f);
+        middle3.transform.localRotation = Quaternion.Euler(3f * _values[2], 0f, 0f);
+
+        ring1.transform.localRotation = Quaternion.Euler(_values[3], 0f, 0f);
+        ring2.transform.localRotation = Quaternion.Euler(2f * _values[3], 0f, 0f);
+        ring3.transform.localRotation = Quaternion.Euler(3f * _values[3], 0f, 0f);
+
+        pinky1.transform.localRotation = Quaternion.Euler(_values[4], 0f, 0f);
+        pinky2.transform.localRotation = Quaternion.Euler(2f * _values[4], 0f, 0f);
+        pinky3.transform.localRotation = Quaternion.Euler(3f * _values[4], 0f, 0f);
+        
+        if (_values[0] + _values[1] + _values[2] + _values[3] + _values[4] >= 300)
+        {
+            Grabbing = true;
+        }
+        else
+        {
+            Grabbing = false;
+        }
+        
+    }
+
+    private void ReadData()
+    {
+        // Destroy Thread when exiting Program
+        while (isRunning)
+        {
+            var current = _serialPort.ReadLine();
+            Console.WriteLine(current);
+            string[] readValues = current.Split(ValueSeparator);
+
+            // Check if received values match the default pattern, if not restart loop
+            // This is for the case of receiving a different serial print from the Arduino
+            // otherwise the code could crash
+            if (readValues.Length < ValueAmount || readValues.Length > ValueAmount)
+            {
+                Console.WriteLine("Incorrect Data length or setup");
+                continue;
+            }
+
+            // Parsing all string values of the read values into the float array 'values'
+            _values = Array.ConvertAll(readValues, float.Parse);
+
+            // Pause the thread for x milliseconds to not uselessly waste computing power
+            Thread.Sleep(ThreadSleepInMillis);
+        }
+    }
+    
+    void OnApplicationQuit()
+    {
+        isRunning = false;
+    }
+
+    public static bool IsGrabbing()
+    {
+        return Grabbing;
+    }
+}
---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    public string selectableTag = "Selectable";
+    public Material highLightMaterial;
+    public Transform hand;
+    public Transform handGrabPlaceHolder;
+
+    private Material _selectedMaterial;
+    private bool _holding = false;
+    private Transform _currentHolding;
+    private Transform _selection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = _selectedMaterial;
+            _selection = null;
+        }
+
+        var ray = new Ray(hand.position, hand.forward);
+        Debug.DrawRay(ray.origin, ray.direction, Color.green);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 3))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    _selectedMaterial = selection.GetComponent<Renderer>().material;
+                    selectionRenderer.material = highLightMaterial;
+                }
+
+                _selection = selection;
+                if (!HandController.IsGrabbing() && _holding)
+                {
+                    offHand();
+                    _currentHolding = null;
+                    _holding = false;
+                }
+            }
+        }
+        if (!HandController.IsGrabbing() && _holding)
+        {
+            offHand();
+            _currentHolding = null;
+            _holding = false;
+        }
+    }
+    
+    private void toHand()
+    {
+        _selection.GetComponent<BoxCollider>().enabled = false;
+        _selection.GetComponent<Rigidbody>().useGravity = false;
+        _selection.GetComponent<Rigidbody>().freezeRotation = true;
+        _selection.transform.position = handGrabPlaceHolder.position;
+        _selection.transform.parent = handGrabPlaceHolder.transform;
+    }
+
+    private void offHand()
+    {
+        _currentHolding.GetComponent<BoxCollider>().enabled = true;
+        _currentHolding.transform.parent = null;
+        _currentHolding.GetComponent<Rigidbody>().freezeRotation = false;
+        _currentHolding.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
